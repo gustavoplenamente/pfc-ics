a8d44660b6c15abfef0160adc9fcba0861eb4656
Gustavo Plenamente <gplenamente@inoa.com.br>
2021-09-25 21:06:58 -0300

    refactor: extract methods and rename vars

--- ics.py
+++ ics.py
@@ -10,7 +10,7 @@ def get_dataset(dataset_name: str):
 
 class ICS:
 
-    def __init__(self, laplace_smoothing=0.01, omega=0.5, news_shared_threshold=4):
+    def __init__(self, laplace_smoothing=0.01, omega=0.5, news_shared_threshold=5):
 
         self.__users = get_dataset("users.csv")
         self.__news = get_dataset("2_news.csv")
@@ -20,6 +20,54 @@ class ICS:
         self.__smoothing = laplace_smoothing
         self.__omega = omega
         self.__news_shared_threshold = news_shared_threshold
+
+    def fit(self, test_size=0.3):
+        """
+        Training step: calculate the parameters of each user based on Implicit Crowd Signals
+        """
+        self.__init_params(test_size)
+
+        i = 0
+        unique_users = self.__train_news_users["id_social_media_account"].unique()
+        total = len(unique_users)
+
+        for user_id in unique_users:
+            i = i + 1
+            print("", end="Training progress: {0:.2f}%\r".format(float((i / total) * 100)), flush=True)
+            self.__calculate_user_reputation(user_id)
+
+        self.__assess()
+
+    def predict(self, news_id):
+        """
+        Classify news based on Implicit Crowd Signals
+        """
+
+        users_who_shared_the_news = self.__get_users_who_shared(news_id)
+
+        productAlphaN = 1.0
+        productUmAlphaN = 1.0
+        productBetaN = 1.0
+        productUmBetaN = 1.0
+
+        for user_id in users_who_shared_the_news:
+            i = self.__users.loc[self.__users["id_social_media_account"] == user_id].index[0]
+            productAlphaN = productAlphaN * self.__users.at[i, "probAlphaN"]
+            productUmBetaN = productUmBetaN * self.__users.at[i, "probUmBetaN"]
+
+        # bayesian inference
+        reputation_news_tn = (self.__omega * productAlphaN * productUmAlphaN) * 100
+        reputation_news_fn = ((1 - self.__omega) * productBetaN * productUmBetaN) * 100
+
+        # calculate probability grade of the prediction
+        total = reputation_news_tn + reputation_news_fn
+
+        if reputation_news_tn >= reputation_news_fn:
+            prob = reputation_news_tn / total
+            return 0, prob  # news classified as not fake
+        else:
+            prob = reputation_news_fn / total
+            return 1, prob  # news classified as fake
 
     def __init_params(self, test_size):
 
@@ -41,17 +89,20 @@ class ICS:
         self.__qtd_V = self.__news["ground_truth_label"].value_counts()[0]
         self.__qtd_F = self.__news["ground_truth_label"].value_counts()[1]
 
-        # filter users which are in both train and test datasets
+        # filter users which are also in test dataset
         self.__train_news_users = self.__train_news_users[
             self.__train_news_users["id_social_media_account"].isin(self.__test_news_users["id_social_media_account"])]
 
-        # initialize users parameters
+        self.__init_users_params()
+
+    def __init_users_params(self):
         totR = 0
         totF = 0
         alphaN = totR + self.__smoothing
         umAlphaN = ((totF + self.__smoothing) / (self.__qtd_F + self.__smoothing)) * (self.__qtd_V + self.__smoothing)
         betaN = (umAlphaN * (totR + self.__smoothing)) / (totF + self.__smoothing)
         umBetaN = totF + self.__smoothing
+
         probAlphaN = alphaN / (alphaN + umAlphaN)
         probUmAlphaN = 1 - probAlphaN
         probBetaN = betaN / (betaN + umBetaN)
@@ -70,17 +121,15 @@ class ICS:
         unique_news_ids = self.__test_news_users["id_news"].unique()
 
         for news_id in unique_news_ids:
-            # get the ids of users who posted the news represented by 'news_id'
-            users_which_shared_the_news = list(
-                self.__news_users["id_social_media_account"].loc[self.__news_users["id_news"] == news_id])
+            users_who_shared_the_news = self.__get_users_who_shared(news_id)
 
             productAlphaN = 1.0
             productUmAlphaN = 1.0
             productBetaN = 1.0
             productUmBetaN = 1.0
 
-            for user_id in users_which_shared_the_news:
-                user_prob_alpha, user_prob_um_beta = self.get_user_probs(user_id)
+            for user_id in users_who_shared_the_news:
+                user_prob_alpha, user_prob_um_beta = self.__get_user_probs(user_id)
 
                 productAlphaN = productAlphaN * user_prob_alpha
                 productUmBetaN = productUmBetaN * user_prob_um_beta
@@ -99,80 +148,44 @@ class ICS:
         print(confusion_matrix(gt, predicted_labels))
         print(accuracy_score(gt, predicted_labels))
 
-    def fit(self, test_size=0.3):
-        """
-        Training step: calculate the parameters of each user based on Implicit Crowd Signals
-        """
-        self.__init_params(test_size)
+    def __calculate_user_reputation(self, user_id):
+        # get the labels of news posted by user
+        labels_of_news_shared_by_user = list(
+            self.__train_news_users["ground_truth_label"].loc[
+                self.__train_news_users["id_social_media_account"] == user_id
+                ]
+        )
 
-        i = 0
-        unique_users = self.__train_news_users["id_social_media_account"].unique()
-        total = len(unique_users)
+        # calculate the user opinion matrix
+        totR = labels_of_news_shared_by_user.count(0)
+        totF = labels_of_news_shared_by_user.count(1)
+        alphaN = totR + self.__smoothing
+        umAlphaN = ((totF + self.__smoothing) / (self.__qtd_F + self.__smoothing)) * (
+                self.__qtd_V + self.__smoothing)
+        betaN = (umAlphaN * (totR + self.__smoothing)) / (totF + self.__smoothing)
+        umBetaN = totF + self.__smoothing
 
-        for user_id in unique_users:
-            i = i + 1
-            print("", end="Training progress: {0:.2f}%\r".format(float((i / total) * 100)), flush=True)
+        # calculate user reputation
+        probAlphaN = alphaN / (alphaN + umAlphaN)
+        probUmAlphaN = 1 - probAlphaN
+        probBetaN = betaN / (betaN + umBetaN)
+        probUmBetaN = 1 - probBetaN
 
-            # get the labels of news posted by each user
-            news_shared_by_user = list(self.__train_news_users["ground_truth_label"].loc[
-                                        self.__train_news_users["id_social_media_account"] == user_id])
+        self.__users.loc[self.__users["id_social_media_account"] == user_id, "probAlphaN"] = probAlphaN
+        self.__users.loc[self.__users["id_social_media_account"] == user_id, "probBetaN"] = probBetaN
+        self.__users.loc[self.__users["id_social_media_account"] == user_id, "probUmAlphaN"] = probUmAlphaN
+        self.__users.loc[self.__users["id_social_media_account"] == user_id, "probUmBetaN"] = probUmBetaN
+        self.__users.loc[self.__users["id_social_media_account"] == user_id, "isAccurate"] = \
+            len(labels_of_news_shared_by_user) >= self.__news_shared_threshold
 
-            # calculate the opinion matrix of each user
-            totR = news_shared_by_user.count(0)
-            totF = news_shared_by_user.count(1)
-            alphaN = totR + self.__smoothing
-            umAlphaN = ((totF + self.__smoothing) / (self.__qtd_F + self.__smoothing)) * (
-                        self.__qtd_V + self.__smoothing)
-            betaN = (umAlphaN * (totR + self.__smoothing)) / (totF + self.__smoothing)
-            umBetaN = totF + self.__smoothing
+    def __get_users_who_shared(self, id_news):
+        return list(
+            self.__news_users["id_social_media_account"].loc[
+                self.__news_users["id_news"] == id_news
+            ]
+        )
 
-            # calculate probabilities for each user
-            probAlphaN = alphaN / (alphaN + umAlphaN)
-            probUmAlphaN = 1 - probAlphaN
-            probBetaN = betaN / (betaN + umBetaN)
-            probUmBetaN = 1 - probBetaN
-            self.__users.loc[self.__users["id_social_media_account"] == user_id, "probAlphaN"] = probAlphaN
-            self.__users.loc[self.__users["id_social_media_account"] == user_id, "probBetaN"] = probBetaN
-            self.__users.loc[self.__users["id_social_media_account"] == user_id, "probUmAlphaN"] = probUmAlphaN
-            self.__users.loc[self.__users["id_social_media_account"] == user_id, "probUmBetaN"] = probUmBetaN
-            self.__users.loc[self.__users["id_social_media_account"] == user_id, "isAccurate"] = \
-                len(news_shared_by_user) > self.__news_shared_threshold
-
-        self.__assess()
-
-    def predict(self, id_news):
-        """
-        Classify news based on Implicit Crowd Signals
-        """
-
-        users_which_shared_the_news = list(
-            self.__news_users["id_social_media_account"].loc[self.__news_users["id_news"] == id_news])
-
-        productAlphaN = 1.0
-        productUmAlphaN = 1.0
-        productBetaN = 1.0
-        productUmBetaN = 1.0
-
-        for userId in users_which_shared_the_news:
-            i = self.__users.loc[self.__users["id_social_media_account"] == userId].index[0]
-            productAlphaN = productAlphaN * self.__users.at[i, "probAlphaN"]
-            productUmBetaN = productUmBetaN * self.__users.at[i, "probUmBetaN"]
-
-        # bayesian inference
-        reputation_news_tn = (self.__omega * productAlphaN * productUmAlphaN) * 100
-        reputation_news_fn = ((1 - self.__omega) * productBetaN * productUmBetaN) * 100
-
-        # calculate probability grade of the prediction
-        total = reputation_news_tn + reputation_news_fn
-
-        if reputation_news_tn >= reputation_news_fn:
-            prob = reputation_news_tn / total
-            return 0, prob  # news classified as not fake
-        else:
-            prob = reputation_news_fn / total
-            return 1, prob  # news classified as fake
-
-    def get_user_probs(self, userId):
+    def __get_user_probs(self, userId):
         i = self.__users.loc[self.__users["id_social_media_account"] == userId].index[0]
         user_prob_alpha = self.__users.at[i, "probAlphaN"]
         user_prob_um_beta = self.__users.at[i, "probUmBetaN"]
